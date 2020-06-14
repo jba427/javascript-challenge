0b0572bddf30a85e7574626671fb77c82e207cfa
unknown <jadams@peoplenetonline.com>
2020-06-14 17:57:00 -0500

    5 filters works

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -29,14 +29,65 @@ function runEnter() {
     
     // Select the input element and get the raw HTML node
     var inputElement = d3.select("#datetime");
+    var inputElement2 = d3.select("#state");
+    var inputElement3 = d3.select("#city");
+    var inputElement4 = d3.select("#country");
+    var inputElement5 = d3.select("#shape");
   
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
+    var inputValue2 = inputElement2.property("value");
+    var inputValue3 = inputElement3.property("value");
+    var inputValue4 = inputElement4.property("value");
+    var inputValue5 = inputElement5.property("value");
   
-    console.log(inputValue);
+    console.log(inputValue.length);
+    console.log(inputValue2.length);
+    console.log(inputValue3.length);
+    console.log(inputValue4.length);
+    console.log(inputValue5.length);
   
-    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
-  
+    //filter by 1st criteria
+    if (inputValue.length >= 1) {
+      var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+    }
+    else {
+      var filteredData = tableData;
+    };
+
+    //filter by 2nd criteria
+    if (inputValue2.length >= 1) {
+      var filteredData = filteredData.filter(filteredData => filteredData.state === inputValue2);
+    }
+    else {
+      var filteredData = filteredData;
+    };
+
+    //filter by 3rd criteria
+    if (inputValue3.length >= 1) {
+      var filteredData = filteredData.filter(filteredData => filteredData.city === inputValue3);
+    }
+    else {
+      var filteredData = filteredData;
+    };
+
+    //filter by 4th criteria
+    if (inputValue4.length >= 1) {
+      var filteredData = filteredData.filter(filteredData => filteredData.country === inputValue4);
+    }
+    else {
+      var filteredData = filteredData;
+    };
+
+    //filter by 5th criteria
+    if (inputValue5.length >= 1) {
+      var filteredData = filteredData.filter(filteredData => filteredData.shape === inputValue5);
+    }
+    else {
+      var filteredData = filteredData;
+    };
+
+
     console.log(filteredData);
 
     //Clear the table before refreshing filtered data on the screen
