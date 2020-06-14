f2c821641e7f37d4d6c504fa5ee77770b9b9842f
unknown <jadams@peoplenetonline.com>
2020-06-14 16:43:24 -0500

    level-1 display filtered table commit

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,3 +2,41 @@
 var tableData = data;
 
 // YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+// Select the button
+var button = d3.select("#filter-btn");
+
+// Select the form
+var form = d3.select("#filters");
+
+// Create event handlers 
+button.on("click", runEnter);
+form.on("submit",runEnter);
+
+// Complete the event handler function for the form
+function runEnter() {
+
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // Select the input element and get the raw HTML node
+    var inputElement = d3.select("#datetime");
+  
+    // Get the value property of the input element
+    var inputValue = inputElement.property("value");
+  
+    console.log(inputValue);
+  
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+  
+    console.log(filteredData);
+
+    filteredData.forEach((filteredData) => {
+        var row = tbody.append("tr");
+        Object.entries(filteredData).forEach(([key, value]) => {
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });   
+};
